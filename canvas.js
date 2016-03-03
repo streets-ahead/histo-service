2edd22cdc30519dced881595cf4bb6fced825981
Streets Ahead <devs@sts-ahead.com>
2016-03-02 23:56:56 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,31 +1,32 @@
-var Canvas = require('canvas'),
-	http = require('http'),
-	util = require('util'),
-	url = require('url');
+var http = require('http');
+var Canvas = require('canvas');
+var url = require('url');
 
-var HEIGHT = 25;
-var WIDTH = 20;
-var DEFAULT_COLOR = "rgba(50, 50, 50, 1.000)";
+var WIDTH = 30;
+var HEIGHT = 60;
+var DEFAULT_COLOR = '#58C6C2';
 
 function render(canvas, numbers, color) { 
 	ctx = canvas.getContext('2d');
 	
 	var biggestNumber = Math.max.apply(null, numbers);
+
 	var currentX = 10 + WIDTH/2;
 	var lineHeight = 0;
 	
+	ctx.beginPath();
+	ctx.moveTo(currentX, HEIGHT + 10);
 	ctx.lineWidth = WIDTH;
 	ctx.strokeStyle = color;
 	
-	ctx.beginPath();
-	ctx.moveTo(currentX, HEIGHT + 10);
-
  	numbers.forEach(function(it, ind) {
 		lineHeight = (HEIGHT+10) - (HEIGHT * (it / biggestNumber));
+
 		ctx.lineTo(currentX, lineHeight);
 		currentX += WIDTH;
-		ctx.moveTo(currentX, HEIGHT + 10);
+  	ctx.moveTo(currentX, HEIGHT + 10);
 	});
+	
 	ctx.closePath();
 	ctx.stroke();
 }
@@ -34,25 +35,19 @@ http.createServer(function (req, res) {
 	var q = url.parse(req.url, true).query;
 	try {
 		var numbers = JSON.parse(q.numbers);
-		var canvas = canvas = new Canvas(numbers.length*WIDTH,60);
-		console.log(q.color);
-		q.color = q.color ? '#' + q.color : DEFAULT_COLOR;
-		render( canvas, numbers, q.color );
-	  	res.writeHead(200, { 'Content-Type': 'image/png' });
-		canvas.toBuffer(function(err, buf) {
-			if(!err) {
-				res.write(buf);
-				res.end();
-			} else {
-				throw 'buffer error';
-			}
+		var canvas = new Canvas(numbers.length*WIDTH + 20, HEIGHT+10);
+		var color = q.color ? '#' + q.color : DEFAULT_COLOR;
+		render( canvas, numbers, color );
+	  	
+	  res.writeHead(200, { 'Content-Type': 'image/png' });
+		canvas.toBuffer(function(err, buf){
+			res.write(buf);
+			res.end();
 		});
 	} catch (e) {
-		console.log(e);
+		console.error(e);
 		res.writeHead(500);
 		res.end('Internal Server Error');
 	}
 }).listen(8888);
 console.log('Server started on port 8888');
-
-
